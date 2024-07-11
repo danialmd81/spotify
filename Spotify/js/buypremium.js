121035ef5d0c4c8a7ba1f88bebb67f02d9da20e4
sobhan <sobhanaghajani893@gmail.com>
2024-07-11 13:02:40 +0330

    update Premium Users page

--- Spotify/js/buypremium.js
+++ Spotify/js/buypremium.js
@@ -1,24 +1,43 @@
-const getStartedBtn = document.querySelector(".btn-primary");
+const getStartedBtn = document.querySelectorAll(".btn-primary");
 
-getStartedBtn.addEventListener("click", () => {
-    fetch('/wallet/take', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({ amount: 9.99 })
+getStartedBtn.forEach(button => {
+    button.addEventListener("click", () => {
+        const cardDiv = button.closest(".card-Premium");
+        const priceParagraph = cardDiv.querySelector("p");
+        const priceText = priceParagraph.textContent;
+        const priceMatch = priceText.match(/\$([0-9]+\.[0-9]+)/);
+        const value = parseFloat(priceMatch[0].slice(1))
+        let duration = 0;
+        if (value < 10) {
+            duration = 30;
+        }
+        else if (value < 20) {
+            duration = 60;
+        }
+        else {
+            duration = 90;
+        }
+
+        fetch('/wallet/take', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ amount: value, duration: duration })
+        })
+            .then(response => response.json())
+            .then(data => {
+                const walletMessage = document.getElementById("walletMessage");
+                if (data.error) {
+                    walletMessage.textContent = data.error;
+                    walletMessage.style.backgroundColor = "#ffcccb";
+                    walletMessage.style.color = "#d8000c";
+                } else {
+                    walletMessage.textContent = data.message;
+                    window.location.href = '/addToPremium';
+                }
+                walletMessage.style.display = 'block';
+            });
     })
-        .then(response => response.json())
-        .then(data => {
-            const walletMessage = document.getElementById("walletMessage");
-            if (data.error) {
-                walletMessage.textContent = data.error;
-                walletMessage.style.backgroundColor = "#ffcccb";
-                walletMessage.style.color = "#d8000c";
-            } else {
-                walletMessage.textContent = data.message;
-                window.location.href = '/addToPremium';
-            }
-            walletMessage.style.display = 'block';
-        });
+
 });
